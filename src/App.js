30829186dc8a8a466783733b86fbe8a0b1c0cef7
Mahbub2001 <imahbubturza@gmail.com>
2022-11-12 02:14:21 +0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import {RouterProvider} from 'react-router-dom'
+import router from './Routes/Routes/Routes';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className='max-w-[1440px] mx-auto'>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='mx-5'>
+            this is home
+        </div>
+    );
+};
+
+export default Home;
