df14136a47dc0eb605ca2a78858388866079cbc8
Mahbub2001 <imahbubturza@gmail.com>
2022-11-13 00:31:05 +0600

    testimonial

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -4,6 +4,7 @@ import DentalCare from '../DentalCare/DentalCare';
 import InfoCards from '../InfoCards/InfoCards';
 import MakeAppoinment from '../MakeAppoinment/MakeAppoinment';
 import Services from '../Services/Services';
+import Testimonial from '../Testimonial/Testimonial';
 
 const Home = () => {
     return (
@@ -13,6 +14,7 @@ const Home = () => {
             <Services/>
             <DentalCare/>
             <MakeAppoinment/>
+            <Testimonial/>
         </div>
     );
 };
