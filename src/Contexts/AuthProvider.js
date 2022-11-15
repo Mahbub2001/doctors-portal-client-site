22705c35c24cb182e79a790f4e73355e1da7635f
Mahbub2001 <imahbubturza@gmail.com>
2022-11-15 11:56:21 +0600

    context

---
+++ src/Contexts/AuthProvider.js
@@ -0,0 +1,22 @@
+import React, { createContext } from 'react';
+import {getAuth} from 'firebase/auth'
+import app from '../firebase/firebase.config';
+
+const AuthContext = createContext();
+const auth = getAuth(app)
+
+const AuthProvider = ({children}) => {
+
+
+    const authInfo = {
+
+    }
+
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'react-day-picker/dist/style.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "react-day-picker/dist/style.css";
+import AuthProvider from "./Contexts/AuthProvider";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
 
